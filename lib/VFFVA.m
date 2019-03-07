661cf482af81fba117f56df08111602c74b07660
marouenbg <marouen.b.guebila@gmail.com>
2019-03-07 22:00:59 +0000

    working VFFVQ matlab

--- lib/VFFVA.m
+++ lib/VFFVA.m
@@ -15,11 +15,17 @@ if nargin<6
    schedule='dynamic'; 
 end
 if nargin<7
-    nChunk=50
+    nChunk=50;
+end
+[system,cmdout]=system('mpirun');
+if isequal(cmdout,'/bin/bash: mpirun: command not found')
+    error(['MPI and/or CPLEX nont installed, please follow the install guide'...
+    'or use the quick install script']);
 end
 
 system(['export OMP_SCHEDUELE=' schedule num2str(nChunk)])
-system(['mpirun -np' num2str(nCores) '--bind-to ' num2str(memAff) '-x OMP_NUM_THREADS=' num2str(nThreads)...
-    './veryfastFVA ' model num2str(scaling)]);
+setenv('PATH', [getenv('PATH') ':home/marouen_b_guebila/open-mpi/bin'])
+[status,cmdout]=system(['mpirun -np ' num2str(nCores) ' --bind-to ' num2str(memAff) ' -x OMP_NUM_THREADS=' num2str(nThreads)...
+    ' ./veryfastFVA ' model ' ' num2str(scaling)]);
 
 end
