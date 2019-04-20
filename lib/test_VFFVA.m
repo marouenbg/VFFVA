4b9ecd598ec02dd96e2b2854ca6abbda810d4c28
marouenbg <marouen.b.guebila@gmail.com>
2019-04-20 18:09:09 +0000

    added plot in test

--- lib/test_VFFVA.m
+++ lib/test_VFFVA.m
@@ -1,5 +1,9 @@
 nCores=1;
 nThreads=4;
+%add cobratoolbox and VFFVA to path
+addpath(genpath('cobratoolbox'))
+addpath(genpath('VFFVA'))
+cd('VFFVA/lib')
 model='../data/models/Ecoli_core/Ecoli_core.mps';
 
 %.mps model
@@ -16,8 +20,20 @@ model=LPProblemtRNACoupled90;
 [minFlux,maxFlux]=VFFVA(nCores, nThreads, model);
 
 %compare to FVA from COBRA toolbox
+changeCobraSolver('ibm_cplex');
 optPercentage=90;
 [minFluxFVA,maxFluxFVA]=fluxVariability(ecoli, optPercentage);
-plot(minFluxVFFVA,minFluxFVA,'o')
+figure;
+plot([minFluxVFFVA;maxFluxVFFVA],[minFluxFVA;maxFluxFVA],'o','MarkerSize',8,...
+    'MarkerFaceColor','b')
 hold on;
-plot([-40 100],[-40 100])
+%set(gca, 'XScale', 'log', 'YScale', 'log')
+plot([-40 1000],[-40 1000],'LineWidth',1);
+ax=gca;
+ax.FontSize=12;
+xlabel('VFFVA fluxes','FontSize',18); ylabel('FVA fluxes','FontSize',18);
+title('Comparion of VFFVA and FVA fluxes','fontsize',18);
+xlim([-40 1000]);ylim([-40 1000])
+legend('data points','y=x')
+grid on
+
